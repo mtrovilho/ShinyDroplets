f3b1ff842e1b54693d2af53bc763f5e97c483bc4
Marcos Trovilho <marcos@trovilho.com>
2012-12-01 15:40:34 -0200

    add sublime text support

---
+++ sublimeTextDrop/SublimeTextDropController.h
@@ -0,0 +1,16 @@
+//
+//  SublimeTextDropController.h
+//  ShinyDroplets
+//
+//  Created by Marcos Trovilho on 12/1/12.
+//  Copyright (c) 2012 mtrovilho. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "baseDroplet.h"
+
+@interface SublimeTextDropController : baseDroplet {
+
+}
+
+@end
---
+++ sublimeTextDrop/SublimeTextDropController.m
@@ -0,0 +1,17 @@
+//
+//  SublimeTextDropController.m
+//  ShinyDroplets
+//
+//  Created by Marcos Trovilho on 12/1/12.
+//  Copyright (c) 2012 mtrovilho. All rights reserved.
+//
+
+#import "SublimeTextDropController.h"
+
+@implementation SublimeTextDropController
+
+- (NSString *) appName{
+    return @"Sublime Text 2";
+}
+
+@end
